a60242dec7948fa0fb1f60fa281a13bcfab5e1e6
aloks <sarkarsworld@gmail.com>
2026-04-05 19:11:43 +0530

    Initial commit

---
+++ src/main/java/MainMethodEx.java
@@ -0,0 +1,9 @@
+public class MainMethodEx {
+
+
+
+    void main(String[] args) {
+        System.out.println("This is not the main method");
+    }
+
+}
---
+++ src/main/java/OutputLine.java
@@ -0,0 +1,7 @@
+public class OutputLine {
+
+    static void main() {
+        System.out.println("this is a traditional line printed on console.");
+        IO.println("this is a line printed on console.");
+    }
+}
